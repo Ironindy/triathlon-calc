79f8785d0de68dd3e4178742ae3da871d5ff436c
Ironindy <hakee22@gmail.com>
2026-02-10 16:00:20 +0700

    Add files via upload

---
+++ src/Splash.jsx
@@ -0,0 +1,25 @@
+export default function Splash() {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        inset: 0,
+        backgroundColor: "#FFFFFF",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        zIndex: 9999
+      }}
+    >
+      <img
+        src="/icon2.png"
+        alt="철인계산기"
+        style={{
+          width: "40vw",
+          maxWidth: "180px",
+          height: "auto"
+        }}
+      />
+    </div>
+  );
+}
